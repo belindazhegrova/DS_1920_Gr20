454621624b9f41b8ab9b137339251737c44ed234
belindazhegrova <53189831+belindazhegrova@users.noreply.github.com>
2020-03-28 21:29:02 +0100

    Update Frequency_command.java

--- Faza_e_pare/src/Frequency_command.java
+++ Faza_e_pare/src/Frequency_command.java
@@ -6,21 +6,15 @@ import java.util.Comparator;
 import java.util.List;
 public class Frequency_command {
 	
-	  static void frequency_code(String str)
-      {
+	  static void frequency_code(String str) {
    	  
    	   
 	    	int counter[] = new int[256];
-	        
-	        
-	        
-	        
-	        str = str.replaceAll("\\s","");
+	         str = str.replaceAll("\\s","");
 	        char symbol='#';
 	        String len="";
 	        
-	         for (int i = 0; i < str.length(); i++)
-	         {
+	         for (int i = 0; i < str.length(); i++) {
 	            counter[(int) str.charAt(i)]++;
 	            len=len+symbol;
 	         }
@@ -29,10 +23,8 @@ public class Frequency_command {
 	      
 	         
 	        int t;
-	        for (int j = 0; j < 256; j++) 
-	        {
-	            if (counter[j] != 0 && counter[j]!=' ')
-	            {
+	        for (int j = 0; j < 256; j++)  {
+	            if (counter[j] != 0 && counter[j]!=' ') {
 	       
 	            	double l=(((double)counter[j]/str.length())*100);
 	                   System.out.printf( (char) j + "  " + counter[j]  + " "+ "%.2f",l);
@@ -45,22 +37,18 @@ public class Frequency_command {
 	       System.out.println("");
 	       
 	      
-	        for (int j = 0; j < 256; j++) 
-	        {
-	            if (counter[j] != 0 && counter[j]!=' ')
-	            {
+	        for (int j = 0; j < 256; j++)  {
+	            if (counter[j] != 0 && counter[j]!=' ') {
 	            	 t=(int)(((double)counter[j]/str.length())*100-((double)counter[j]/str.length())*80);
 	            	
 	         
-	                    double q=(((double)counter[j]/str.length())*100);
+	                   double q=(((double)counter[j]/str.length())*100);
 	                   System.out.printf((char) j + ": ["+ len.substring(0,t) +"   ]"+"%.2f",q);
 	                   System.out.print("%");
-	                  System.out.println("");
+	                   System.out.println("");
 	                 
-	            }
-	        }  
-	       
+	               }
+	          }  
+	       }
 
-      }
-
-}
+          }
