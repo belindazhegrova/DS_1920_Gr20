9303ce0cdb283292238a300a6c6fd4d4131f6800
BleonaKonjuhi <52915332+BleonaKonjuhi@users.noreply.github.com>
2020-06-02 22:32:18 +0200

    Changes

--- Projekti_Siguri/src/hs_password.java
+++ Projekti_Siguri/src/hs_password.java
@@ -32,12 +32,7 @@ public class  hs_password{
 
 
 /*import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
 
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
 
 public class hs_password {
 	
