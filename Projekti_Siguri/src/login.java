67fac68a6f5322df8ef02dd0e1b802c2bc8fbeb5
belindazhegrova <53189831+belindazhegrova@users.noreply.github.com>
2020-06-02 22:33:21 +0200

    update login

--- Projekti_Siguri/src/login.java
+++ Projekti_Siguri/src/login.java
@@ -7,17 +7,16 @@ public class login {
 	static void login(String name) throws SQLException, NoSuchAlgorithmException, InvalidKeySpecException {
 		 java.io.Console console = System.console();
 	      String pass1 = console.readLine("Jepni fjalekalimin: ");
-	     
-	  
 	      
-	      dbconnect connect = new dbconnect();
+              dbconnect connect = new dbconnect();
 	      String pass2= connect.login(name);
 	  
 	 if( hs_password.checkPassword(pass1,pass2)) {
 	     System.out.println("Token: "+"true");
+
 	     }
 	}
-	
+
 	static String token() {
 		return null;
 	}
