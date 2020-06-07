7f919bdf5360566dc42993ecafd903de815d12ab
Aurola Zeneli <52915274+aurolazeneli@users.noreply.github.com>
2020-06-07 20:20:47 +0200

    Update read_message.java

--- Projekti_Siguri/src/read_message.java
+++ Projekti_Siguri/src/read_message.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,10 +9,14 @@ import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.AlgorithmParameterSpec;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
 import java.util.Base64;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -107,6 +112,57 @@ public class read_message {
 			read(str);
 		}
 			 
+	}
+	
+	static void readmesstoken(String str) throws InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, IOException, ParserConfigurationException, SAXException, SignatureException {
+		String[] emessage = str.split("\\.");
+		String part1 = emessage[0] + "."+emessage[1] + "."+ emessage[2]+"."+emessage[3];
+		read1(part1);
+	
+		byte[] s =  Base64.getDecoder().decode(emessage[4]);
+ 		String Sender = new String(s);
+ 		
+		System.out.println("\nDerguesi: "+Sender);
+	
+		try {sigv(emessage[3],emessage[4], emessage[5]) ;
+			System.out.println("Nenshkrimi: Valid");
+			
+		} catch (Exception e) {
+			System.out.println("Nenshkrimi: mungon celesi publik " + "'"+ Sender +"'");
+		}
+		
+	}
+static boolean sigv(String token,String name,String mess) throws ParserConfigurationException, SAXException, IOException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, SignatureException {
+		
+	
+		byte[] s = Base64.getDecoder().decode(token);
+		byte[] payload = Base64.getDecoder().decode(name);
+		String dpayload = new String(payload);
+		String name1 = dpayload;
+	
+		String dp=new String(s) ;
+		
+		File file = new File("keys/" + name1 + ".pub.xml");
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        Document doc = docBuilder.parse(file);
+        doc.getDocumentElement().normalize();
+        String moduliS = doc.getElementsByTagName("Modulus").item(0).getTextContent();
+        String exponentAsString = doc.getElementsByTagName("Exponent").item(0).getTextContent();
+        BigInteger modulus = new BigInteger(1,Base64.getDecoder().decode(moduliS));
+        BigInteger exponent = new BigInteger(1,Base64.getDecoder().decode(exponentAsString));
+        RSAPublicKeySpec keySpec = new RSAPublicKeySpec(modulus, exponent);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PublicKey publicKey = keyFactory.generatePublic(keySpec);
+
+
+        Signature Signature1 = Signature.getInstance("SHA256withRSA");
+        Signature1.initVerify(publicKey);
+        Signature1.update(dp.getBytes("UTF-8"));
+    
+        byte[] sBytes = Base64.getDecoder().decode(mess);
+       
+        return Signature1.verify(sBytes);
 	}
 }
  
