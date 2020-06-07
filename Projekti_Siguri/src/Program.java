3b3528749b59fbc0c368c9cc2ac4fa6fb854fe70
Aurola Zeneli <52915274+aurolazeneli@users.noreply.github.com>
2020-06-07 21:27:45 +0200

    Update Program.java

--- Projekti_Siguri/src/Program.java
+++ Projekti_Siguri/src/Program.java
@@ -7,6 +7,27 @@ public class Program {
 
 			 dbconnect connect = new dbconnect();
 			switch (args[0]) {
+					case "frequency": Frequency_command.frequency_code(args[1]);
+			 break;
+			 
+			 case "morse-code":switch(args[1]) {
+							case "encode": if (args[2].equalsIgnoreCase("--audio"))
+									Morse_command.run(args[3]);
+								else System.out.println(Morse_command.Morse_Encode(args[2]));
+							break;
+							case "decode": System.out.println(Morse_command.Morse_Decode(args[2]));
+							break;
+							default:{
+						System.out.println("USAGE: java Program morse-code encode|decode <plaintext|ciphertext> or with alias: ds morse-code encode|decode <plaintext|ciphertext>");
+		                System.exit(1);}
+		}
+			 break;
+			 case "four-square":switch(args[1]) {
+							case "encrypt":  System.out.println(Four_Square_command.encrypt(args[2], args[3], args[4]));
+							break;
+							case "decrypt":  System.out.println(Four_Square_command.decrypt(args[2], args[3], args[4]));
+							break;}
+		
 		
                         case "create-user":try {
                         	if(args[2].equals("read -s"))
