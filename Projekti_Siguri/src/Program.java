8e144589ad45d1d7e026757b852b7413e41dfbca
belindazhegrova <53189831+belindazhegrova@users.noreply.github.com>
2020-06-07 20:23:15 +0200

    Update Program.java

--- Projekti_Siguri/src/Program.java
+++ Projekti_Siguri/src/Program.java
@@ -1,12 +1,23 @@
 
+
 public class Program {
 	public static void main(String[] args) throws Exception {
 		try {
+			  
+
+			 dbconnect connect = new dbconnect();
 			switch (args[0]) {
 		
-                        case "create-user": create_user.create1(args[1]);
+                        case "create-user":try {
+                        	if(args[2].equals("read -s"))
+                            	create_user.create2(args[1]);
+							
+						} catch (Exception e) {
+							create_user.create1(args[1]);
+						}
                          break;
                         case "delete-user": delete_user.delete1(args[1]);
+                       connect.delete(args[1]);
                         break;
                         case "export-key": 
                         	switch(args[1]) {
@@ -55,12 +66,47 @@ public class Program {
                         	
                         break;
                                        
-                        case "write-message":try{write_message.write1(args[1], args[2], args[3]);}
-                        catch (Exception e) {
-							write_message.write2(args[1],args[2]);
-						}
+                        case "write-message":
+							try {
+								if (args[3].equals("--sender")) {
+									write_message.write3(args[1], args[2], args[4]);}
+								else {
+									write_message.write1(args[1], args[2], args[3]);}
+							
+							}
+							catch (Exception e) {
+								System.out.println(write_message.write(args[1],args[2]));
+							}
+							
+						
                         break;
-                        case "read-message": read_message.read1(args[1]);
+                        case "read-message":
+                        	
+                        		int count = 0;
+
+                        	    for(int i=0; i < args[1].length(); i++)
+                        	    {    if(args[1].charAt(i) == '.')
+                        	            count++;
+                        	    }
+								if (count==5) {
+									read_message.readmesstoken(args[1]);}
+								else {
+									read_message.read1(args[1]);
+							
+							}
+						
+                        	
+                        	
+                        break;
+                        case "login":try {
+                        	if(args[2].equals("read -s"))
+                            	login.login1(args[1]);
+							
+						} catch (Exception e) {
+							login.login(args[1]);
+						}
+                         break;
+                        case "status":Status.verify(args[1]);
                         break;
                         default:
                         {
@@ -71,9 +117,9 @@ public class Program {
 			}
 			catch (ArrayIndexOutOfBoundsException e ) { 
 				
-		               System.out.println("Gabim2");
+		             System.out.println("Gabim2");
 				System.exit(1);
-			          }
+			         }
 	                         }
                                
 
